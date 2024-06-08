7a94823335ed387e4395c035d62f5025db13238d
Bekbolot <justworking8888@gmail.com>
2024-06-08 15:36:37 +0600

    ads

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,39 +1,39 @@
 import { Route, Routes } from 'react-router-dom';
 import './scss/app.scss';
-
-import { Login, SignUp } from 'Components';
-import MainPage from 'routes/MainPage/MainPage';
 import Main from 'layout/Main/Main';
-import FullScreen from 'layout/FullScreen/FullScreen';
-import Catalog from 'routes/Catalog/Catalog';
-import SingleProduckt from 'routes/SingleProduckt/SingleProduckt';
-import Cart from 'routes/Cart/Cart';
-import OrderPlacing from 'routes/OrderPlacing/OrderPlacing';
-import Contact from 'routes/Contact/Contact';
-import AboutUs from 'routes/AboutUs/AboutUs';
-import AllRevies from 'routes/AllRevies/AllRevies';
-import Shop from 'routes/Shop/Shop';
-import Menu from 'routes/Menu/Menu';
-import TablePage from 'routes/Table/TablePage';
+import { Suspense, lazy } from 'react';
+import { Spin } from 'antd';
+
+const SingleProduckt = lazy(() => import('./routes/SingleProduckt/SingleProduckt'))
+const MainPage = lazy(() => import('routes/MainPage/MainPage'))
+const Cart = lazy(() => import('routes/Cart/Cart'))
+const OrderPlacing = lazy(() => import('routes/OrderPlacing/OrderPlacing'))
+const Contact = lazy(() => import('routes/Contact/Contact'))
+const AboutUs = lazy(() => import('routes/AboutUs/AboutUs'))
+const AllRevies = lazy(() => import('routes/AllRevies/AllRevies'))
+const Shop = lazy(() => import('routes/Shop/Shop'))
+const Menu = lazy(() => import('routes/Menu/Menu'))
+const TablePage = lazy(() => import('routes/Table/TablePage'))
+const Login = lazy(() => import('./Components/Login/Login'))
+const SignUp = lazy(() => import('./Components/SignUp/SignUp'))
 
 function App() {
 
   return (
     <Routes>
-      <Route path='/login' element={<Login />} />
-      <Route path='/signUp' element={<SignUp />} />
+      <Route path='/login' element={<Suspense fallback={<Spin spinning={true} fullscreen />}><Login /></Suspense>} />
+      <Route path='/signUp' element={<Suspense fallback={<Spin spinning={true} fullscreen />}><SignUp /></Suspense>} />
       <Route path='/' element={<Main />}>
-        <Route path='/' element={<MainPage />} />
-        {/* <Route path='/catalog' element={<Catalog />} /> */}
-        <Route path='/food/:id' element={<SingleProduckt />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="orders" element={<OrderPlacing />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='about' element={<AboutUs />} />
-        <Route path='revies' element={<AllRevies />} />
-        <Route path='shop' element={<Shop />} />
-        <Route path='catalog' element={<Menu />} />
-        <Route path='table/:id' element={<TablePage />} />
+        <Route path='/' element={<Suspense fallback={<Spin spinning={true} fullscreen />}><MainPage /></Suspense>} />
+        <Route path='/food/:id' element={<Suspense fallback={<Spin spinning={true} fullscreen />}><SingleProduckt /></Suspense>} />
+        <Route path="cart" element={<Suspense fallback={<Spin spinning={true} fullscreen />}><Cart /></Suspense>} />
+        <Route path="orders" element={<Suspense fallback={<Spin spinning={true} fullscreen />}><OrderPlacing /></Suspense>} />
+        <Route path='contact' element={<Suspense fallback={<Spin spinning={true} fullscreen />}><Contact /></Suspense>} />
+        <Route path='about' element={<Suspense fallback={<Spin spinning={true} fullscreen />}><AboutUs /></Suspense>} />
+        <Route path='revies' element={<Suspense fallback={<Spin spinning={true} fullscreen />}><AllRevies /></Suspense>} />
+        <Route path='shop' element={<Suspense fallback={<Spin spinning={true} fullscreen />}><Shop /></Suspense>} />
+        <Route path='catalog' element={<Suspense fallback={<Spin spinning={true} fullscreen />}><Menu /></Suspense>} />
+        <Route path='table/:id' element={<Suspense fallback={<Spin spinning={true} fullscreen />}><TablePage /></Suspense>} />
       </Route>
       <Route path='*' element={<main className={'errorPage'}><p>Неверный адрес</p></main>} />
     </Routes>
